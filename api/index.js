28c4beaca34fd966f81a872ff8c920aae1e13e07
Md. Robin <robinhosan75@gmail.com>
2024-01-29 01:49:16 +0600

    complete lecture 11

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,8 +15,21 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 // app.get("/", (req, res) => res.send("hi"));
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, nex) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
